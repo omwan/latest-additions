eb3cc58a8b1a6eb79d0529225cebb05e3ffafbb5
Olivia Wan <wan.o@husky.neu.edu>
2018-03-30 14:25:49 -0400

    adding swagger configs

---
+++ src/main/java/com/omwan/latestadditions/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.omwan.latestadditions;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/api/**"))
+                .build()
+                .apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .contact(new Contact("Olivia Wan", "https://github.com/omwan", "wan.o@husky.neu.edu"))
+                .description("Auto-generate playlist of latest additions to a set of playlists")
+                .title("Latest Additions")
+                .build();
+    }
+}
