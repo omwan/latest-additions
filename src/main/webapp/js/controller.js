bfa52f469dc9939ad4126536d08612927a438278
Olivia Wan <wan.o@husky.neu.edu>
2018-03-30 14:03:38 -0400

    adding angular material design to ui

--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -1,29 +1,51 @@
 app.controller('controller', ['$scope', '$http', function ($scope, $http) {
 
     $scope.playlists = null;
+    $scope.selectedPlaylists = [];
+
+    $scope.playlistSort = "$index";
+    $scope.playlistSortReverse = false;
+
+    $scope.playlistFilter = "";
 
     var _getPlaylists = function () {
-        $http.get("/api/spotify/playlists").then(function (response) {
+        $http.get("/api/spotify/playlists")
+        .then(function (response) {
             $scope.playlists = response.data;
         });
     };
 
     $scope.loadMorePlaylists = function () {
         if ($scope.playlists) {
-            $http.get("/api/spotify/playlists", {
-                "params": {
-                    "offset": $scope.playlists.offset + $scope.playlists.limit,
-                    "limit": $scope.playlists.limit
-                }
-            }).then(function (response) {
-                var origPlaylists = $scope.playlists.items;
-                $scope.playlists = response.data;
-                origPlaylists.push.apply(origPlaylists, $scope.playlists.items);
-                $scope.playlists.items = origPlaylists;
-            })
+           $http.get("/api/spotify/playlists", {
+               "params": {
+                   "offset": $scope.playlists.offset + $scope.playlists.limit,
+                   "limit": $scope.playlists.limit
+               }
+           })
+           .then(function (response) {
+               var origPlaylists = $scope.playlists.items;
+               $scope.playlists = response.data;
+               origPlaylists.push.apply(origPlaylists, $scope.playlists.items);
+               $scope.playlists.items = origPlaylists;
+           });
         } else {
-            return null
+            return null;
         }
+    };
+
+    $scope.toggle = function (item, list) {
+        var idx = list.indexOf(item);
+        if (idx > -1) {
+            list.splice(idx, 1);
+        }
+        else {
+            list.push(item);
+        }
+    };
+
+    $scope.exists = function (item, list) {
+        return list.indexOf(item) > -1;
     };
 
     _getPlaylists();
